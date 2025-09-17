a08f07dc7e437225718b9a1b5c43bb68f2050da9
PHATHUY\troll <trollergamehuydk@gmail.com>
2025-09-17 12:47:54 +0700

    create folder structure and initialize prisma

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common'
+import { SharedModule } from '~/shared/shared.module'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+@Module({
+  imports: [SharedModule],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
