a2ed632859856fad9d48c6d0e6ef432c2795c41e
PhucHong-6113 <phuongdubu@gmail.com>
2025-09-20 17:31:05 +0700

    [ADD] add test for auth and app controller

---
+++ src/routes/auth/auth.service.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { PrismaService } from '../../shared/services/prisma.service';
+import { HashingService } from '../../shared/services/hashing.service';
+import { JwtService } from '@nestjs/jwt';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findFirst: jest.fn(),
+            },
+          },
+        },
+        {
+          provide: HashingService,
+          useValue: {
+            comparePassword: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+            verify: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
