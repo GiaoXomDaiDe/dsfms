7ad444c146a8225ad31774b8e7bb1826fcbe5837
PHATHUY\troll <trollergamehuydk@gmail.com>
2025-11-08 15:34:09 +0700

    REFACTOR: enhance logging for ZodSerializationException with request details and serialized issues

--- src/shared/filters/http-exception.filter.ts
+++ src/shared/filters/http-exception.filter.ts
@@ -1,5 +1,6 @@
 import { ArgumentsHost, Catch, HttpException, Logger } from '@nestjs/common'
 import { BaseExceptionFilter } from '@nestjs/core'
+import type { Request } from 'express'
 import { ZodSerializationException } from 'nestjs-zod'
 import { ZodError as ZodErrorV4 } from 'zod/v4'
 
@@ -10,8 +11,13 @@ export class HttpExceptionFilter extends BaseExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     if (exception instanceof ZodSerializationException) {
       const zodError = exception.getZodError()
+      const httpContext = host.switchToHttp()
+      const request = httpContext.getRequest<Request>()
+      const route = request ? `${request.method} ${request.originalUrl ?? request.url}` : 'Unknown route'
+      console.log('Log thử: ', JSON.stringify(exception.initCause(), null, 2))
       if (zodError instanceof ZodErrorV4) {
-        this.logger.error(`ZodSerializationException: ${zodError.message}`)
+        const serializedIssues = JSON.stringify(zodError.issues, null, 2)
+        this.logger.error(`ZodSerializationException at ${route}`, serializedIssues)
       }
     }
 
