90497e37ee72e8f67e5d546e8115cb16bf631d04
PHATHUY\troll <trollergamehuydk@gmail.com>
2025-11-11 21:01:45 +0700

    REFACTOR: streamline AssessmentModule and MediaModule by removing unused providers and ensuring proper exports

--- src/routes/assessment/assessment.module.ts
+++ src/routes/assessment/assessment.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common'
+import { EmailModule } from '../email/email.module'
+import { MediaModule } from '../media/media.module'
 import { AssessmentController } from './assessment.controller'
-import { AssessmentService } from './assessment.service'
 import { AssessmentRepo } from './assessment.repo'
-import { NodemailerService } from '../email/nodemailer.service'
-import { MediaService } from '../media/media.service'
-import { PdfConverterService } from '~/shared/services/pdf-converter.service'
-import { S3Service } from '~/shared/services/s3.service'
+import { AssessmentService } from './assessment.service'
 
 @Module({
+  imports: [EmailModule, MediaModule],
   controllers: [AssessmentController],
-  providers: [AssessmentService, AssessmentRepo, NodemailerService, MediaService, PdfConverterService, S3Service],
+  providers: [AssessmentService, AssessmentRepo],
   exports: [AssessmentService, AssessmentRepo]
 })
 export class AssessmentModule {}
