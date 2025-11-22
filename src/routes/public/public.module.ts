87035d0f9837e98c8e7ed2ef184c33085cb04074
PHATHUY\troll <trollergamehuydk@gmail.com>
2025-11-22 15:28:15 +0700

    [UPDATE] add PublicTrainee module, controller, service, and DTO for managing active trainees

--- src/routes/public/public.module.ts
+++ src/routes/public/public.module.ts
@@ -5,10 +5,12 @@ import { PublicDepartmentController } from './department/public-department.contr
 import { PublicDepartmentService } from './department/public-department.service'
 import { PublicRoleController } from './role/public-role.controller'
 import { PublicRoleService } from './role/public-role.service'
+import { PublicTraineeController } from './trainee/public-trainee.controller'
+import { PublicTraineeService } from './trainee/public-trainee.service'
 
 @Module({
-  controllers: [PublicDepartmentController, PublicRoleController, PublicCourseController],
-  providers: [PublicDepartmentService, PublicRoleService, PublicCourseService],
-  exports: [PublicDepartmentService, PublicRoleService, PublicCourseService]
+  controllers: [PublicDepartmentController, PublicRoleController, PublicCourseController, PublicTraineeController],
+  providers: [PublicDepartmentService, PublicRoleService, PublicCourseService, PublicTraineeService],
+  exports: [PublicDepartmentService, PublicRoleService, PublicCourseService, PublicTraineeService]
 })
 export class PublicModule {}
